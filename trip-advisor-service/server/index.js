98dba962f1fc461f070fffc8505d74d490231ffe
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-13 20:30:52 -0700

    Setup Server

---
+++ trip-advisor-service/server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+app.use(express.static('../public'));
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log("Error Starting server")
+  } else {
+    console.log("Server Running on Port: ", port)
+  }
+});
