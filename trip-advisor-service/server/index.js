f2acc840256ce6eee4edb6e7aa051c5981ed07e1
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-15 11:44:54 -0700

    Add GET route for ID

--- trip-advisor-service/server/index.js
+++ trip-advisor-service/server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('../db/index.js');
 
 const app = express();
 
@@ -8,6 +9,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('../public'));
+
+app.get('/api/showcase/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then((data) => res.status(200).send(data))
+    .catch((err) => {
+      console.log('Error GETTING by ID', err);
+      res.status(400).send();
+    });
+});
 
 app.listen(port, (err) => {
   if (err) {
