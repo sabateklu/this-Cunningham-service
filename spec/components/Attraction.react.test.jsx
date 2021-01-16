fa70ab66331a7aef89d8115d4bcdb779874e7563
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-16 11:32:46 -0700

    Initial render tests for Attraction Component

--- spec/components/Attraction.react.test.jsx
+++ spec/components/Attraction.react.test.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
 // import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 // import { shallow, mount, render } from 'enzyme';
 import Attraction from '../../src/components/Attraction';
+import Header from '../../src/components/Header';
+import Images from '../../src/components/Images';
+import Overview from '../../src/components/Overview';
 
 // const Attraction = () => (
 //   <div className="attraction">Test</div>
 // );
 
 describe('Attraction', () => {
-  it('should render without throwing an error', () => {
-    expect(shallow(<Attraction />).contains(<div className="attraction">Test</div>)).toBe(true);
+  it('this.state.current should equal null on initial render', () => {
+    expect(shallow(<Attraction />).state().current).toBeNull();
+  });
+  it('should render Loading Div on initial render', () => {
+    expect(shallow(<Attraction />).containsMatchingElement(
+      <div className="loading">Loading...</div>,
+    )).toBeTruthy();
+  });
+  it('should only have one child element on initial render', () => {
+    expect(shallow(<Attraction />).children()).toHaveLength(1);
   });
 });
