543a185289cbe8499caf574fe94f8674872631e4
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-17 12:49:36 -0700

    JEST - Improve listing component should render a form

--- spec/components/Overview.react.test.jsx
+++ spec/components/Overview.react.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Attraction from '../../src/components/Attraction';
+import ImproveListing from '../../src/components/ImproveListing';
 import Overview from '../../src/components/Overview';
 import data from './mockData';
 
@@ -22,5 +23,21 @@ describe('<Overview />', () => {
       },
     });
     expect(status.text()).toEqual('Closed:');
+    wrapper.unmount();
+  });
+  test('Improve listing component should render', () => {
+    expect(wrapper.find('.overview').containsMatchingElement(
+      ImproveListing,
+    )).toBe(true);
+    wrapper.unmount();
+  });
+  describe('<ImproveListing />', () => {
+    let improveListing;
+    beforeEach(() => {
+      improveListing = shallow(<ImproveListing />);
+    });
+    test('it should render a form', () => {
+      expect(improveListing.find('form').type()).toBe('form');
+    });
   });
 });
