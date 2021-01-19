efa4090d33fabdba4b715b3dda1a0d43d54a92ee
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-19 15:30:00 -0700

    Jest - routes, add tests to /api/showcase route

--- spec/server.test.js
+++ spec/server.test.js
@@ -1,8 +1,11 @@
 /**
  * @jest-environment node
  */
+import 'regenerator-runtime/runtime';
+
 const request = require('supertest');
 const express = require('express');
+const mongoose = require('mongoose');
 const showcase = require('../server/showcase');
 
 const app = express();
@@ -13,9 +16,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/', showcase);
 
 describe('showcase Routes', () => {
-  test('/api/showcase route works', (done) => {
-    request(app).get('/api/showcase')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
+  afterEach(() => {
+    mongoose.connection.close();
+  });
+  test('/api/showcase route works', async (done) => {
+    const response = await request(app).get('/api/showcase');
+    expect(response.status).toBe(200);
+    expect(response.headers['content-type']).toContain('json');
+    expect(response.body[0].overview.description).toBeTruthy();
+    expect(typeof response.body[0].overview).toEqual('object');
+    expect(Array.isArray(response.body[0].relativeRanking)).toBeTruthy();
+    console.log(response.body[0])
+    done();
   });
 });
