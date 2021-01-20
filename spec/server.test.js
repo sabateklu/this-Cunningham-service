5abea5ec576ac002effaa76dbf4dfbc25fd24edc
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-19 21:29:00 -0700

    Circle

--- spec/server.test.js
+++ spec/server.test.js
@@ -40,7 +40,7 @@ describe('showcase Routes', () => {
   });
   test('/api/showcase/:id GET route', async (done) => {
     const response = await request(app).get('/api/showcase');
-    const id = response.body[0]._id; /* eslint-disable-line */
+    const id = response.body[0]._id; /* eslint-disable-line no-underscore-dangle */
     request(app).get(`/api/showcase/${id}`)
       .expect(200)
       .end((err, res) => {
