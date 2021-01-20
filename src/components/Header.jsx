184c8196e5d50f1adac585db8f6b5e5a17ea06de
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-20 12:50:39 -0700

    CSS modules - header module refactored to use css modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FaRegHeart } from 'react-icons/fa';
 import { FiShare } from 'react-icons/fi';
+import css from '../styles/header.module.css';
 
 const Header = ({ current, updateHeartHover, likeHover }) => (
-  <div className="header">
-    <h3 className="header-lg">{current.attractionTitle}</h3>
-    <div className="reviews-like">
-      <p className="reviews">{current.reviews} Reviews </p>
+  <div className={css.header}>
+    <h3 className={css['header-lg']}>{current.attractionTitle}</h3>
+    <div className={css['reviews-like']}>
+      <p className={css.reviews}>{current.reviews} Reviews </p>
       <FaRegHeart className="icon" color={likeHover ? 'red' : 'black'} size={20} onMouseEnter={updateHeartHover} onMouseLeave={updateHeartHover} />
     </div>
-    <div className="ranking-share">
-      <p className="ranking">
+    <div className={css['ranking-share']}>
+      <p className={css.ranking}>
         #{current.relativeRanking[0]} of {current.relativeRanking[1]} things to do in {current.city}
       </p>
       <FiShare size={25} />
     </div>
-    <p className="type">{current.attractionType}</p>
+    <p className={css.type}>{current.attractionType}</p>
   </div>
 );
 
