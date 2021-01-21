d61e5967fc48a925ad499f0bff8a232016a22aab
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-21 14:07:24 -0700

    Add bubbles function for calculating url based on rating

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,20 @@ import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { FiShare } from 'react-icons/fi';
 import css from '../styles/header.module.css';
 
+const bubbles = function (rating) {
+  if (typeof rating !== 'number') {
+    return;
+  }
+  function roundHalf(num) {
+    return (Math.floor(num * 2) / 2).toFixed(1);
+  }
+  const modifiedRating = roundHalf(rating);
+  if (Number(modifiedRating) <= 5.0) {
+    return `http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/s${modifiedRating}-MCID-5.svg`; /* eslint-disable-line consistent-return */
+  }
+  return null; /* eslint-disable-line consistent-return */
+};
+
 const Header = ({
   current, updateHeartHover, likeHover, updateLikeStatus,
 }) => (
