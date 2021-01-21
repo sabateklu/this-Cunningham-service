6353cb65e42d71eef115b44641e1a0703a4ce4ba
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-21 15:34:15 -0700

    Finish overview component css with icons

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FaRegClock } from 'react-icons/fa';
+import { FiMapPin } from 'react-icons/fi';
 import { CgSandClock } from 'react-icons/cg';
 import ImproveListing from './ImproveListing';
 import css from '../styles/overview.module.css';
@@ -11,13 +12,21 @@ const Overview = ({
   <div className={css.overview}>
     <h4 className={css['overview-header']}>Overview</h4>
     <p className={css.description}>{overview.description}</p>
-    <p className={css['open-closed']}><strong>{overview.isOpen ? 'Open Now' : 'Closed'}:</strong> Hours Here</p>
-    <p className={css.duration}>
-      <strong>Suggested Duration: </strong>{overview.suggestedDuration} minutes
-    </p>
-    <div className={css['address-container']}>
-      <div className={css.address}><strong>Address: </strong></div>
-      <p className={css.address}> {overview.address}</p>
+    <div className={css.clock}>
+      <FaRegClock size={20} /><p className={css['open-closed']}><strong>{overview.isOpen ? 'Open Now' : 'Closed'}:</strong> Hours Here</p>
+    </div>
+    <div className={css.time}>
+      <CgSandClock className={css.timeIcon} size={25} />
+      <p className={css.duration}>
+        <strong>Suggested Duration: </strong>{overview.suggestedDuration} minutes
+      </p>
+    </div>
+    <div className={css.addressWithIcon}>
+      <FiMapPin className={css.addressIcon} size={25} />
+      <div className={css['address-container']}>
+        <div className={css.address}><strong>Address:</strong></div>
+        <p className={css.address}>{overview.address}</p>
+      </div>
     </div>
     <ImproveListing
       form={form}
