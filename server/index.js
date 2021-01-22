58011b6a42d279e5bf883f8358edd01cdedef798
Chris_Cunningham <this.cunningham@gmail.com>
2021-01-22 12:58:22 -0700

    PRoxy work

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const showcase = require('./showcase');
 
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
